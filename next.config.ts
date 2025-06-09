34a684d942720a96b872bc261f17b671b722e0de
Blackrose-blackhat <musharafz2k3@gmail.com>
2025-06-09 09:30:18 +0530

    minified constructor issue

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,21 @@ const nextConfig: NextConfig = {
     // your project has type errors.
     ignoreBuildErrors: true,
   },
- 
+  webpack: (config, { dev, isServer }) => {
+    if (!dev) {
+      // Configure Terser to keep function and class names
+      config.optimization.minimizer.forEach((minimizer: any) => {
+        if (minimizer.constructor.name === 'TerserPlugin') {
+          minimizer.options.terserOptions = {
+            ...minimizer.options.terserOptions,
+            keep_fnames: true,      // Keep function names
+            keep_classnames: true,  // Keep class names
+          };
+        }
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
