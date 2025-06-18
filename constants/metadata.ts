99abb1f21d549385270c1e4a37048ef76831f9c9
Blackrose-blackhat <musharafz2k3@gmail.com>
2025-06-18 13:14:34 +0530

    opengraph

---
+++ constants/metadata.ts
@@ -0,0 +1,14 @@
+import { Logo } from "@/public/images";
+
+export const siteMetaData = {
+  title: "Neurobase",
+  theme: "dark",
+  siteUrl: "https://db-gpt-seven.vercel.app",
+  siteLogo: Logo.src,
+  image: Logo.src,
+  socialBanner: "/images/opengraph.png", // assuming it's in /public root
+  locale: "en-US",
+  language: "en-us",
+  description:
+    "Neurobase is a local-first, AI-powered database assistant that lets you interact with Postgres, MongoDB, and more using natural language — securely, with zero data leaks.",
+};
